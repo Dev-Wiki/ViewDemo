8eb5f3c8ed914683a43821a1980abb33e360e78f
zyz_live <bozhou2010@live.com>
2016-07-07 23:31:13 +0800

    增加操作

--- app/src/main/java/net/devwiki/viewdemo/MainActivity.java
+++ app/src/main/java/net/devwiki/viewdemo/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button bottomBtn;
     private Button clearBtn;
 
+    private TextView stateTv;
+
     private RadioButton translationRb;
     private RadioButton scrollRb;
 
@@ -29,13 +31,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.text_view);
+        textView.setOnClickListener(this);
         leftBtn = (Button) findViewById(R.id.btn_left);
+        leftBtn.setOnClickListener(this);
         rightBtn = (Button) findViewById(R.id.btn_right);
+        rightBtn.setOnClickListener(this);
         topBtn = (Button) findViewById(R.id.btn_top);
+        topBtn.setOnClickListener(this);
         bottomBtn = (Button) findViewById(R.id.btn_bottom);
+        bottomBtn.setOnClickListener(this);
         clearBtn = (Button) findViewById(R.id.btn_clear);
+        clearBtn.setOnClickListener(this);
         translationRb = (RadioButton) findViewById(R.id.translation_rb);
         scrollRb = (RadioButton) findViewById(R.id.scroll_rb);
+        stateTv = (TextView) findViewById(R.id.state_tv);
         textView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -52,6 +61,77 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_left:
+                clickLeft();
+                showState();
+                break;
+            case R.id.btn_top:
+                clickTop();
+                showState();
+                break;
+            case R.id.btn_right:
+                clickRight();
+                showState();
+                break;
+            case R.id.btn_bottom:
+                clickBottom();
+                showState();
+                break;
+            case R.id.btn_clear:
+                clickClear();
+                showState();
+                break;
+        }
+    }
 
+    private void showState() {
+        String state = "x:" + textView.getX() + ",y:" + textView.getY()
+                + "\ntop:" + textView.getTop() + ",left:" + textView.getLeft() + ",bottom:" + textView.getBottom() + ",right:" + textView.getRight()
+                + "\ntranslationX:" + textView.getTranslationX()
+                + "\ntranslationY:" + textView.getTranslationY()
+                + "\nscrollX:" + textView.getScrollX() + ",scrollY:" + textView.getScrollY();
+        stateTv.setText(state);
+    }
+
+    private void clickLeft() {
+        if (translationRb.isChecked()) {
+            textView.setTranslationX(textView.getTranslationX() - 10);
+        } else {
+            textView.scrollBy(10, 0);
+        }
+    }
+
+    private void clickRight() {
+        if (translationRb.isChecked()) {
+            textView.setTranslationX(textView.getTranslationX() + 10);
+        } else {
+            textView.scrollBy(-10, 0);
+        }
+    }
+
+    private void clickTop() {
+        if (translationRb.isChecked()) {
+            textView.setTranslationY(textView.getTranslationY() - 10);
+        } else {
+            textView.scrollBy(0, 10);
+        }
+    }
+
+    private void clickBottom() {
+        if (translationRb.isChecked()) {
+            textView.setTranslationY(textView.getTranslationY() + 10);
+        } else {
+            textView.scrollBy(0, -10);
+        }
+    }
+
+    private void clickClear() {
+        if (translationRb.isChecked()) {
+            textView.setTranslationX(0);
+            textView.setTranslationY(0);
+        } else {
+            textView.scrollTo(0,0);
+        }
     }
 }
